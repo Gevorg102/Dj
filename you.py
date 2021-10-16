873e0cb7783606f0b01d64795c88d93673563c2a
Gevorg102 <87714014+Gevorg102@users.noreply.github.com>
2021-10-16 20:36:01 +0400

    Delete you.py

--- you.py
+++
@@ -1,9 +0,0 @@
-import pytube
-
-
-url = 'https://www.youtube.com/watch?v=cjny7ykmcUE&ab_channel=NEFFEX'
-
-youtube = pytube.YouTube(url)
-audio = youtube.streams.filter(only_audio=True).first()
-
-audio.download()
